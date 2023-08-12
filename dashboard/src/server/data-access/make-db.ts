b75bba723d0367d11648a2a73137c827bc0b40d0
huytran17 <alpha.lloyd1368@gmail.com>
2023-08-12 18:27:29 +0700

    make connect to db

--- dashboard/src/server/data-access/make-db.ts
+++ dashboard/src/server/data-access/make-db.ts
@@ -1,1 +1,16 @@
-export {};
+import mongoose from "mongoose";
+
+export default async function makeDb() {
+  try {
+    const connection_string = makeConnectionString();
+    await mongoose.connect(connection_string);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function makeConnectionString() {
+  const { MONGO_HOST, MONGO_PORT, MONGO_DATABASE } = process.env;
+
+  return `mongodb://${MONGO_HOST}:${MONGO_PORT}/${MONGO_DATABASE}`;
+}
