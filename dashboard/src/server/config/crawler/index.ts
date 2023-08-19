0522b2b97362332b4c4054b29e6d71e88b8600cf
huytran17 <alpha.lloyd1368@gmail.com>
2023-08-19 14:05:24 +0700

    crawler config

---
+++ dashboard/src/server/config/crawler/index.ts
@@ -0,0 +1,51 @@
+import Crawler from "crawler";
+
+export default class NewsCrawler {
+  static crawler_instance: NewsCrawler;
+
+  constructor() {
+    if (NewsCrawler.crawler_instance) {
+      return NewsCrawler.crawler_instance;
+    }
+
+    NewsCrawler.crawler_instance = this;
+    return NewsCrawler.crawler_instance;
+  }
+
+  static getInstance(): NewsCrawler {
+    if (NewsCrawler.crawler_instance) {
+      return NewsCrawler.crawler_instance;
+    }
+
+    NewsCrawler.crawler_instance = new NewsCrawler();
+    return NewsCrawler.crawler_instance;
+  }
+
+  createCrawler({
+    options,
+  }: {
+    options: Crawler.CreateCrawlerOptions;
+  }): Crawler {
+    const crawler = new Crawler({
+      ...options,
+      callback: (
+        error: Error,
+        res: Crawler.CrawlerRequestResponse,
+        done: Function
+      ) => {
+        if (error) {
+          console.error(error);
+          return done();
+        }
+
+        done();
+        return res.$;
+      },
+    });
+
+    return crawler;
+  }
+}
+
+const crawler = NewsCrawler.getInstance();
+export { crawler };
